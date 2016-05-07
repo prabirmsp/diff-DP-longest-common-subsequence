ba741fbe484a5ae5b2a804d56403dcc41d82b79e
Prabir Pradhan <prabirmsp@gmail.com>
2016-05-06 22:30:58 -0500

    init commit

---
+++ diff.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define MATCH 0
+#define INSTERT 1
+#define DELETE 2
+
+typedef struct {
+	int cost;
+	int parent;
+} cell;
+ 
+char * lcss(char * s, char * t) {
+	cell * m = cell[strlen(s)+1][strlen(t)+1];
+}
+
+
+
+int main() {
+
+	return 0;
+}
